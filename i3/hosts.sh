556b81543661acdda29a1a10488bab6c243aeff2
Zach Collier <zamicol@gmail.com>
2016-07-05 16:45:53 -0600

    add host settings, 12 hour clock

---
+++ i3/hosts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#######
+#######
+#######
+#Put i3 host specific configs in this file.
+
+#Work computer
+if [[ $(hostname -s) = coll* ]]; then
+  xrandr --output DP2 --auto --right-of eDP1
+  xrandr --output HDMI1 --auto --right-of DP2
+fi
