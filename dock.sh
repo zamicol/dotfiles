63cb63a8bacc223c773f0e0de1818cc4c234027b
Zach Collier <zamicol@gmail.com>
2016-12-02 10:18:28 -0700

    add office monitors

--- dock.sh
+++ dock.sh
@@ -8,8 +8,12 @@ source $HOME/.profile
 
 #zlap
 if [[ $(hostname -s) = zlap ]]; then
- xrandr --output DP-2 --auto --right-of LVDS1 --primary
- xrandr --output DP-1 --auto --right-of DP-2
+# xrandr --output DP-2 --auto --right-of LVDS1 --primary
+# xrandr --output DP-1 --auto --right-of DP-2
+
+ #Office
+ xrandr --output DP-2 --auto --right-of LVDS1
+ xrandr --output DP-4 --auto --right-of DP-2 
 fi
 
 #zbox
