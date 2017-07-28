3ae2e876e425397d68226e918ada6a2f3d14f494
Zach Collier <zamicol@gmail.com>
2017-07-28 17:23:21 -0600

    sync script

--- sync.sh
+++ sync.sh
@@ -12,3 +12,16 @@ cd ~/.dotfiles/private
 git add .
 git commit -a -m 'sync script'
 gitpp
+
+
+# Cypher Wallet
+cd $CYPHERWALLET
+git add .
+git commit -a -m 'sync script'
+gitpp
+
+# Cypherpass
+cd $CYPHERPASS
+git add .
+git commit -a -m 'sync script'
+gitpp
