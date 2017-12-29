3abedb1600d4be30535a6074c49025223bf12672
Zach Collier <zamicol@gmail.com>
2017-12-28 17:09:48 -0700

    add vs code (visual studio) repo

--- install.sh
+++ install.sh
@@ -415,7 +415,7 @@ then
 fi
 
 ####################
-# VS Code
+# VS Code (Visual Studio)
 ####################
 
 if [ hash code 2>/dev/null ]; then
@@ -429,7 +429,12 @@ if [ hash code 2>/dev/null ]; then
   code --install-extension HookyQR.beautify
   code --install-extension ronnidc.nunjucks
 else
-  echo >&2 "Code not installed.";
+  echo >&2 "Code not installed. Installing code";
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo apt-get update
+  sudo apt-get install code
 fi
 
 
